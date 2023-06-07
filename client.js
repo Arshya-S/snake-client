f67ffc553b9884bdaa28fd6b1181e90e19d076ba
Arshya Soleimani <arrshya@gmail.com>
2023-06-07 15:55:48 +0000

    established a connection to snake server and allowed for incoming data to come in

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: '50541'
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // console log user that they're dead from idling
+  conn.on('data', () => {
+    console.log('you ded cuz you idled')
+  })
+
+  return conn;
+};
+
+
+module.exports = connect;
